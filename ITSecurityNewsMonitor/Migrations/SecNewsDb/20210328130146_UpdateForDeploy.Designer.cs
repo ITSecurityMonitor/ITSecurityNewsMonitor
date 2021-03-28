5a4b6721ff78b247479a7cd4681a4b2bc7c10fff
Robin Weitzel <robin.weitzel.rw@gmail.com>
2021-03-28 15:17:49 +0200

    Fixed DB migrations

--- ITSecurityNewsMonitor/Migrations/SecNewsDb/20210328130146_UpdateForDeploy.Designer.cs
+++ ITSecurityNewsMonitor/Migrations/SecNewsDb/20210328130146_UpdateForDeploy.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using ITSecurityNewsMonitor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace ITSecurityNewsMonitor.Migrations.SecNewsDb
 {
     [DbContext(typeof(SecNewsDbContext))]
-    partial class SecNewsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210328130146_UpdateForDeploy")]
+    partial class UpdateForDeploy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ITSecurityNewsMonitor/Migrations/SecNewsDb/20210328130146_UpdateForDeploy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITSecurityNewsMonitor.Migrations.SecNewsDb
+{
+    public partial class UpdateForDeploy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
